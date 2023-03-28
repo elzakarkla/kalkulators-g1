3bd262115a50eeaa53e5b38f49c0fe1c5e56a387
elzakarkla <119396340+elzakarkla@users.noreply.github.com>
2023-03-28 08:50:16 +0300

    pievienoju funkcijas

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const plusins = document.querySelector('.js-plus');
 const inputins = document.querySelector('.js-input');
 
-console.log(plusins)
-console.log(inputins)
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const multiply =(cipars1, cipars2)=> {
+    return cipars1 *cipars2
+}
+
+const divide = (cipars1, cipars2)=> {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener("click", ()=> {
+    console.log("mani nospieda")
+})
